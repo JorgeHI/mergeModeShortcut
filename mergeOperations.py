3ef928c44316b0ad0fe0b13bf2d71ccc6900a673
Jorge Hernández <106907504+JorgeHI@users.noreply.github.com>
2022-12-06 14:02:34 +0100

    New Version - 2.0
    Add support for ChannelMerge node and easy installation.

--- mergeOperations.py
+++ mergeOperations.py
@@ -8,15 +8,32 @@ def operationDown():
     """Decrease in 1 the operation value of the merge nodes."""
     updateOperation(-1)
 
-def updateOperation(opValue):
-    """Updates the operation knob of the merge nodes currently selected.
+def checkNodes(nodes, opValue):
+    """Checks the given nodes and update the operation prop with the given value.
 
     Args:
-        opValue(int): Increment value to add to the operation knob.
+        node (list): The list of nuke Nodes to check.
+        opValue (int): Increment value to add to the operation knob.
+
     """
     valuesN = None
-    for nodeObj in nuke.selectedNodes("Merge2"):
+    for nodeObj in nodes:
         if valuesN is None:
             nodeValues = nodeObj.knob("operation").values()
             valuesN = len(nodeValues)
         nodeObj.knob("operation").setValue(((int(nodeObj.knob("operation").getValue()) + opValue) % valuesN))
+
+def updateOperation(opValue):
+    """Updates the operation knob of the merge nodes currently selected.
+
+    Args:
+        opValue (int): Increment value to add to the operation knob.
+    """
+    checkNodes(nuke.selectedNodes("Merge2"), opValue)
+    checkNodes(nuke.selectedNodes("ChannelMerge"), opValue)
+
+
+def installCommands():
+    """Installs the commands in Nuke menu."""
+    nuke.menu("Nodes").addCommand("Actions/mergeOpUp", "mergeOperations.operationUp()", 'ctrl+Up')
+    nuke.menu("Nodes").addCommand("Actions/mergeOpDown", "mergeOperations.operationDown()", 'ctrl+Down')
